ccfe57bc41f5598bfd2a84438511b4c6bd9b4f79
NickMazzotta <nickamazzotta@gmail.com>
2018-06-18 19:31:46 -0400

    implement e to n digits

---
+++ src/main/java/FindEToNthDigit.java
@@ -0,0 +1,30 @@
+package main.java;
+
+import java.text.DecimalFormat;
+
+public class FindEToNthDigit {
+
+	public static void main(String[] args) {
+		
+		try {
+			int places = Integer.parseInt(args[0]);
+			
+			String format = "0.";
+			for (int i=0; i<places; i++) {
+				format = format.concat("#");
+			}
+			
+			DecimalFormat currentFormat = new DecimalFormat(format);
+			String formattedNumber = currentFormat.format(Math.E);
+			
+			System.out.print(formattedNumber + "\n");
+			
+		} catch (NumberFormatException e) {
+			System.out.print("argument is not a number\n");
+		}
+		
+		
+
+	}
+
+}
