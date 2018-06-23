94169f78c5446c2fc5dbeca6d30045f474a9cadb
NickMazzotta <nickamazzotta@gmail.com>
2018-06-23 12:34:49 -0400

    begin setting up ui

---
+++ src/main/java/FloorPlanPriceCalculator.java
@@ -0,0 +1,23 @@
+package main.java;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JPanel;
+
+public class FloorPlanPriceCalculator extends JPanel implements ActionListener {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+
+}
